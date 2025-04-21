6822c316ae3eba785462d65d7661f546cf000378
Pavel <mankov.174@gmail.com>
2025-04-21 23:15:57 +0500

    Initial commit

---
+++ src/test/java/test/BankLoginTest.java
@@ -0,0 +1,56 @@
+package test;
+
+
+import data.DataHelper;
+import data.SQLHelper;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import page.LoginPage;
+
+import static com.codeborne.selenide.Selenide.open;
+import static data.SQLHelper.cleanAuthCode;
+import static data.SQLHelper.cleanDatabase;
+
+public class BankLoginTest {
+    LoginPage loginPage;
+    DataHelper.AuthInfo authInfo = DataHelper.getAuthInfoWithTestData();
+
+    @AfterAll
+    static void tearDownAll() {
+        cleanDatabase();
+    }
+
+    @AfterEach
+    void tearDown(){
+        cleanAuthCode();
+    }
+
+    @BeforeEach
+    void setUp(){
+        loginPage = open("http://localhost:9999", LoginPage.class);
+    }
+
+    @Test
+    void shouldSuccessfulLogin() {
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = SQLHelper.getVerificationCode();
+        verificationPage.validVerify(String.valueOf(verificationCode));
+    }
+
+    @Test
+    void shouldGetErrorNotificationIfLoginWithRandomUserWithoutAddingToBase() {
+        var authInfo = DataHelper.generateRandomUser();
+        loginPage.login(authInfo);
+        loginPage.verifyErrorNotification("Ошибка! \nНеверно указан логин или пароль");
+    }
+
+    @Test
+    void shouldGetErrorNotificationIfLoginWithExistUserAndRandomVerificationCode(){
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = DataHelper.generateRandomVerificationCode();
+        verificationPage.verify(verificationCode.getCode());
+        verificationPage.verifyErrorNotification("Ошибка! \nНеверно указан код! Попробуйте ещё раз.");
+    }
+}
